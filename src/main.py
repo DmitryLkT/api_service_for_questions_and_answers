02c9e4f0285ea342c8c5fb984bf76bdac65a9fcc
DmitryTL <Dmitry.plus1@yandex.ru>
2025-11-15 17:30:08 +0600

    create main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
+from src.data.database import Base, engine
+from src.routers.question_router import router_question
+from src.routers.answer_router import router_answer
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+app.include_router(router_question)
+app.include_router(router_answer)
